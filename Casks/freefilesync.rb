879a8b02be74a785f6862275cf86a6841372bfb6
Captain Harris <67209324+cptharris@users.noreply.github.com>
2025-07-23 21:00:32 -0700

    corrections

--- Casks/freefilesync.rb
+++ Casks/freefilesync.rb
@@ -7,25 +7,22 @@ cask "freefilesync" do
   desc "Folder comparison and synchronization software"
   homepage "https://freefilesync.org/"
 
-  installer script: {
-    executable: "#{staged_path}/FreeFileSync_#{version}.pkg",
-    args:       ["--install"],
-  }
+  pkg "FreeFileSync_#{version}.pkg"
 
   uninstall pkgutil: [
-    "org.freefilesync.pkg.FreeFileSync",
-    "org.freefilesync.pkg.RealTimeSync"
-  ],
-  delete: [
-    "/Applications/FreeFileSync.app",
-    "/Applications/RealTimeSync.app",
-    "/usr/local/bin/freefilesync",
-    "/private/var/folders/8w/rynwtsln12b7g04cjzf34js40000gn/C/org.freefilesync.FreeFileSync"
-  ]
+              "org.freefilesync.pkg.FreeFileSync",
+              "org.freefilesync.pkg.RealTimeSync",
+            ],
+            delete:  [
+              "/Applications/FreeFileSync.app",
+              "/Applications/RealTimeSync.app",
+              "/private/var/folders/8w/rynwtsln12b7g04cjzf34js40000gn/C/org.freefilesync.FreeFileSync",
+              "/usr/local/bin/freefilesync",
+            ]
 
   zap trash: [
     "~/Library/Application Support/FreeFileSync",
-    "~/Library/Preferences/org.freefilesync.FreeFileSync.plist"
+    "~/Library/Preferences/org.freefilesync.FreeFileSync.plist",
     # "~/Library/Saved Application State/org.freefilesync.FreeFileSync.savedState"
   ]
 end
